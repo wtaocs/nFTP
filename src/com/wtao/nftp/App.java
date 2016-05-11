20bde93b3cf53f0c368b1d5b94a2c177a6713605
wtao <wtaocs@qq.com>
2016-05-11 19:53:11 +0800

    工程初始化

---
+++ src/com/wtao/nftp/App.java
@@ -0,0 +1,20 @@
+/**
+ * 
+ */
+package com.wtao.nftp;
+
+/**
+ * @author wtao
+ *
+ */
+public class App {
+
+	/**
+	 * @param args
+	 */
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+
+	}
+
+}
